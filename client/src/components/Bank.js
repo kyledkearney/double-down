a83228308185ed388f4ef008b28720c6ecb644d4
kyledkearney <kyledkearney@gmail.com>
2020-05-20 16:52:00 -0600

    troubleshooting proxy issues with create-react-app

--- client/src/components/Bank.js
+++ client/src/components/Bank.js
@@ -1,22 +1,29 @@
 import React, { Component } from 'react'
 
 export default class Bank extends Component {
-  state = {users: []}
-
-  componentDidMount() {
-    fetch('/users')
-      .then(res => res.json())
-      .then(users => this.setState({ users }));
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <h1>Users</h1>
-        {this.state.users.map(user =>
-          <div key={user.id}>{user.username}</div>
-        )}
-      </div>
-    );
-  }
+  constructor(){
+    super();
+    this.state ={users: []};
+}
+componentDidMount() {
+       fetch('/users')
+         .then(res => {
+             console.log(res);
+             return res.json()
+          })
+         .then(users => { 
+             console.log(users); 
+             this.setState({ users })
+          });
+      }
+render() {
+     return (
+         <div className="App">
+             <h1>Users</h1>
+             {this.state.users.map(user =>
+             <div key={user.id}>user: {user.name} Password: {user.password}</div>
+           )}
+         </div>
+     );
+ }
 }
