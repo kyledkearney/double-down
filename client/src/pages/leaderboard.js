a5625c504f6a6a0e33ec0f22e3c66a238506a8d5
kyledkearney <kyledkearney@gmail.com>
2020-05-23 15:00:07 -0600

    setup pages and app.js

---
+++ client/src/pages/leaderboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Header from '../components/Header'
+
+export default class Leaderboard extends Component {
+render () {
+  return (
+    <>
+      <Header pageTitle='Leaderboard'/>
+    </>
+  )
+
+}
+
+}
